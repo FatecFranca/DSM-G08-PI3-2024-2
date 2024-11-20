955e0c49d8c9be01b4be88aa5f26a48a46dd5dd0
Vinicius de Paula <viniciuusdepaula@gmail.com>
2024-11-20 00:58:29 -0300

    Update de Rotas Produtos

--- back-end/src/routes/produtos.js
+++ back-end/src/routes/produtos.js
@@ -6,6 +6,7 @@ const router = Router()
 router.post('/', controller.create)
 router.get('/', controller.retrieveAll)
 router.get('/:id', controller.retrieveOne)
+router.get('/usuario/:usuarioId', controller.retrieveByUsuarioId);
 router.put('/:id', controller.update)
 router.delete('/:id', controller.delete)
 
